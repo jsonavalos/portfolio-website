bf56deac50009b37a803d883088cf1fe72c1f5d9
jsonavalos <jsonavalos@gmail.com>
2020-12-08 15:59:44 -0800

    Added SSL authentication'

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const bodyParser = require('body-parser')
 
 const port = process.env.PORT || 3000;
@@ -11,9 +12,12 @@ app.get('/', (req,res) => {
 	res.render("home",{name: "Jason Avalos"});
 });
 
+app.get('/.well-known/pki-validation/11D1B573BD9DAD0FC1368F6CAD3BA5A8.txt',(req,res)=>{
+	res.sendFile(path.join(__dirname+ "/views/11D1B573BD9DAD0FC1368F6CAD3BA5A8.txt"));
+});
+
 app.get(/.*/,(req,res)=>{
 	res.redirect('/');
-
 });
 
 app.listen(port,console.log(`Listenning to port: ${port}`)); 
