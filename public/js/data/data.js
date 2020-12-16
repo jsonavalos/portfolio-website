22bdcea9f06544bd0bc8bc6d71977f95d937ceac
jsonavalos <jsonavalos@gmail.com>
2020-12-15 16:18:38 -0800

    Removed image to test loading

---
+++ public/js/data/data.js
@@ -0,0 +1,40 @@
+const projects = [
+    {
+        name: "Velocity Raptor Chrome Extension",
+        link: "https://github.com/cse112-sp20/CalamariCrew",
+        description: "This Chrome Extension was part of a Software Engineering class. Its main purpose is to keep track of sprints using GitHub in an agile team to keep track of velocity. I was in charge of coding the Front-End using HTML, CSS, Bootstrap and JavaScript.",
+        tools: [" Typescript "," Javascript "," HTML "," NodeJS "]
+    },
+    {
+        name: "Portfolio WebApp",
+        link: "https://github.com/jsonavalos/resume",
+        description: "Personal project to create a website using new technologies and learning about Components in React",
+        tools: [" React "," JSX "," HTML "," CSS "]
+    },
+    {
+        name: "LinkUp WebApp",
+        link: "https://github.com/Justin-sd/LinkUp",
+        description: "Team project for a Software Engineering class.As a database specialist, my tasks were to create the SQLite schema, implement it and help front-end coders with the app's design. ",
+        tools: [" Django "," sqlite "," Python "," HTML "," Javascript"," CSS"]
+    },
+    {
+        name: "Link Shortener API",
+        link: "https://github.com/jsonavalos/url-shortener",
+        description: "Small API created to make links shorter.I implemented this API using Flask, Python and MongoDB in order to leverage my skills using a back-end server and learning about the basic functions of CRUD.",
+        tools: [" Flask "," Python "," MongoDB "]
+    },
+    {
+        name: "Email Collector WebApp ",
+        link: "https://github.com/jsonavalos/express-mysql-app",
+        description: "Small Web Application to get users email and store them in a database. I implemented this application using Express, NodeJS and MySQL to practice with a web server for backend sever.",
+        tools: [" Express "," NodeJS "," MySQL "]
+    },
+    {
+        name: "Jason's Website",
+        link: "https://github.com/jsonavalos/electrician-website",
+        description: "This website was created to practice with a back-end server and create a summary of skills",
+        tools: [" Express "," NodeJS "," HTML "," CSS "," Javascript "]
+    }
+]
+
+module.exports = projects;
