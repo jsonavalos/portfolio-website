1ca3e63a627a0122d95e644f38d2917939fb561c
Jason Avalos <javalosm@ucsd.edu>
2024-02-09 16:06:15 -0800

    Removed main image code from home page

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const bodyParser = require('body-parser')
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended: true})); //to read requests 
 app.use('/static', express.static('public'))
