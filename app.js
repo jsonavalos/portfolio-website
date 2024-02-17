45740064329aa330a355eaae535b2fe671b12035
Jason Avalos <javalosm@ucsd.edu>
2024-02-16 16:14:11 -0800

    Added relevant coursework section

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser')
-const programingLanguages = ["java", "python", "kotlin"]
 
-const port = process.env.PORT || 3011;
+const port = process.env.PORT || 3012;
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/static', express.static('public'))
